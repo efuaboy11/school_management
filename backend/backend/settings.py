f66a57d4d1a96a5ea5051bf8778f722127f3e79e
efuaboy11 <iseghohimhene@gmail.com>
2025-10-13 01:15:40 +0100

    changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,7 +74,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://school.amanilightequity.com/",
+    "http://school.amanilightequity.com",
 ]
 
 
