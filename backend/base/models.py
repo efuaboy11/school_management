d5dedb58667cfb3dcc5b0a1861deb1322c0705cc
efuaboy11 <iseghohimhene@gmail.com>
2025-11-11 07:18:14 +0100

    changes

--- backend/base/models.py
+++ backend/base/models.py
@@ -705,7 +705,7 @@ class FavouriteProduct(models.Model):
 class Cart(models.Model):
     user = models.ForeignKey(Users,on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    measurement = models.ForeignKey(ProductMeasurement, on_delete=models.CASCADE)
+    measurement = models.ForeignKey(ProductMeasurement, on_delete=models.CASCADE, blank=True)
     quantity = models.PositiveIntegerField(default=1)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
