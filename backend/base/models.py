7febfe9fb87e7cd73991fe7c0b31ac606eeb36ce
efuaboy11 <iseghohimhene@gmail.com>
2025-11-11 07:19:55 +0100

    changes

--- backend/base/models.py
+++ backend/base/models.py
@@ -679,7 +679,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     rating = models.FloatField(default=0)
-    measurement = models.ManyToManyField(ProductMeasurement, related_name='products', blank=True)
+    measurement = models.ManyToManyField(ProductMeasurement, related_name='products', blank=True, null=True)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -705,7 +705,7 @@ class FavouriteProduct(models.Model):
 class Cart(models.Model):
     user = models.ForeignKey(Users,on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    measurement = models.ForeignKey(ProductMeasurement, on_delete=models.CASCADE, blank=True)
+    measurement = models.ForeignKey(ProductMeasurement, on_delete=models.CASCADE, blank=True, null=True)
     quantity = models.PositiveIntegerField(default=1)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
